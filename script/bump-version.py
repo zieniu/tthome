e62bc1272f7c9aaa2f80bab0949cb7ffe90ecb12
Dawid Zien <zieniu1992@gmail.com>
2021-03-01 08:27:04 +0100

    init

---
+++ script/bump-version.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import subprocess
+from dataclasses import dataclass
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+import generate
+
+
+@dataclass
+class Version:
+    major: int
+    minor: int
+    patch: int
+    beta: int = 0
+    dev: bool = False
+
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.full_patch}'
+
+    @property
+    def full_patch(self):
+        res = f'{self.patch}'
+        if self.beta > 0:
+            res += f'b{self.beta}'
+        if self.dev:
+            res += '-dev'
+        return res
+
+    @classmethod
+    def parse(cls, value):
+        match = re.match(r'(\d+).(\d+).(\d+)(b\d+)?(-dev)?', value)
+        assert match is not None
+        major = int(match[1])
+        minor = int(match[2])
+        patch = int(match[3])
+        beta = int(match[4][1:]) if match[4] else 0
+        dev = bool(match[5])
+        return Version(
+            major=major, minor=minor, patch=patch,
+            beta=beta, dev=dev
+        )
+
+
+def sub(path, pattern, repl, expected_count=1):
+    with open(path) as fh:
+        content = fh.read()
+    content, count = re.subn(pattern, repl, content, flags=re.MULTILINE)
+    if expected_count is not None:
+        assert count == expected_count, f"Pattern {pattern} replacement failed!"
+    with open(path, "wt") as fh:
+        fh.write(content)
+
+
+def write_version(target: str, version: Version):
+    #  version: '1.14.5'  # BETA
+    #  version: '1.14.5'  # STABLE
+    sub(
+        'template/config.yaml',
+        r"  version: '[^']+'  # {}".format(target.upper()),
+        f"  version: '{version}'  # {target.upper()}"
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('new_version', type=str)
+    args = parser.parse_args()
+
+    version = Version.parse(args.new_version)
+    assert not version.dev
+
+    print(f"Bumping to {version}")
+    if version.beta:
+        write_version('beta', version)
+        generate.main(['beta'])
+    else:
+        assert not version.beta
+        write_version('stable', version)
+        write_version('beta', version)
+        generate.main(['stable', 'beta'])
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
---
+++ script/generate.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse
+import yaml
+from pathlib import Path
+from enum import Enum
+import json
+from shutil import copyfile
+import sys
+
+class Channel(Enum):
+    stable = 'stable'
+    beta = 'beta'
+    dev = 'dev'
+
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Generate ESPHome Home Assistant config.json')
+    parser.add_argument('channels', nargs='+', type=Channel, choices=list(Channel))
+    args = parser.parse_args(args)
+
+    root = Path(__file__).parent.parent
+    templ = root / 'template'
+
+    with open(templ / "config.yaml", 'r') as f:
+        config = yaml.safe_load(f)
+
+    copyf = config['copy_files']
+
+    for channel in args.channels:
+        conf = config[f'esphome-{channel.value}']
+        base_image = conf.pop('base_image')
+        dir_ = root / conf.pop('directory')
+        path = dir_ / 'config.json'
+        with open(path, 'w') as f:
+            json.dump(conf, f, indent=2, sort_keys=True)
+
+        for file_, conf_ in copyf.items():
+            copyfile(templ / file_, dir_ / file_)
+
+        path = dir_ / 'FILES ARE GENERATED DO NOT EDIT'
+        with open(path, 'w') as f:
+            f.write("Any edits should be made to the files in the 'template' directory")
+
+        if channel == Channel.dev:
+            path = dir_ / 'build.json'
+            build_conf = {
+                "build_from": {arch: base_image.format(arch=arch) for arch in conf['arch']}
+            }
+            with open(path, 'w') as f:
+                json.dump(build_conf, f, indent=2, sort_keys=True)
+
+        print(f"Wrote {path}")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
